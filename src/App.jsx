f3190c968acf3241f9e34cf25091b95cff9358b8
Ravidhote17 <ravidhote101@gmail.com>
2024-09-15 22:34:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Background from "./components/background";
+import Forground from "./components/Forground";
+
+function App(){
+
+  return(
+
+    <div className="relative w-full h-screen bg-zinc-800">
+      <Background/>
+      <Forground/>
+      
+    </div>
+
+  )
+
+}
+
+export default App;
