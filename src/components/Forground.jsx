5ad0a2e617b1b5ff68cde73fab6096a96b6e976e
Ravidhote17 <ravidhote101@gmail.com>
2024-09-16 02:50:23 +0530

    adding framer motion

--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Card from "./Card";
 
-function Forground(){
+function Forground() {
 
-    return(
+    const ref = useRef(null)
 
-        <div className="fixed top-0 left-0 z-[3] w-full h-full">
-            <Card/>
-        </div>
+  const data = [
+    {
+        desc: "Lorem ipsum dolor sitt, amet consectetur adipisicing.",
+        filesize: ".9mb",
+        close: true,
+        tag: {isOpen: true, tagTitle: "Uplaod", tagColor: "blue"},
+      },
+      {
+        desc: "Lorem ipsum dolor sitt, amet consectetur adipisicing.",
+        filesize: ".9mb",
+        close: true,
+        tag: {isOpen: true, tagTitle: "Download Now", tagColor: "green"},
+      },
+      {
+        desc: "Lorem ipsum dolor sitt, amet consectetur adipisicing.",
+        filesize: ".9mb",
+        close: true,
+        tag: {isOpen: false, tagTitle: "Upload", tagColor: "blue"},
+      },
+  ];
 
-            
-    )
+  return (
+    <div ref={ref} className="p-5 fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap">
+        {data.map((item, index)=>(
+            <Card data={item} reference={ref}/>
+        ))}
+    </div>
+  );
 }
 
-export default Forground
+export default Forground;
