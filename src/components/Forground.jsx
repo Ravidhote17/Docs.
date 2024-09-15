4b49cc98a5d20e91bcdc459c9315b6b7439c1c8b
Ravidhote17 <ravidhote101@gmail.com>
2024-09-16 00:57:36 +0530

    making components

--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Card from "./Card";
 
 function Forground(){
 
     return(
 
         <div className="fixed top-0 left-0 z-[3] w-full h-full">
-            <div className="w-60 h-72 bg-sky-200 rounded-[15px]"></div>
+            <Card/>
         </div>
+
             
     )
 }
